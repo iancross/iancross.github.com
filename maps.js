ae93541fd303ca4755c1b88a5aed23a413e4109e
ian cross <ian.cross@tufts.edu>
2013-03-02 15:21:44 -0500

    testing

--- maps.js
+++ maps.js
@@ -1,3 +1,10 @@
 function initialize(){
-	
+	var mapOptions = {
+		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom =12,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById													     ("map_canvas"),mapOptions);
+		
+	}
 }
