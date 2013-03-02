3afac56153ac0e9b0888419da41098892c503ac5
ian cross <ian.cross@tufts.edu>
2013-03-02 15:29:24 -0500

    testing

--- maps.js
+++ maps.js
@@ -1,7 +1,7 @@
 function initialize(){
 	var mapOptions = {
 		center: new google.maps.LatLng(42.356627,-71.061707),
-		zoom: 12,
+		zoom: 10,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById													     ("map_canvas"),mapOptions);
