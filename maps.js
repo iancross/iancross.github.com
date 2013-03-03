dd30959f3f2904794350ffc9a0ffb8e9430ffdc4
ian cross <ian.cross@tufts.edu>
2013-03-02 23:59:04 -0500

    HARD CODE ADDED

--- maps.js
+++ maps.js
@@ -48,7 +48,7 @@ function renderMap(){
 function plotRed(){
 	var marker, i
 	for (var index in locsRed) { 
-		console.log(index);
+		console.log(locsRed[index][0]);
 		currStat = new google.maps.LatLng(locsRed[index][0]);
 	  	marker = new google.maps.Marker({
 	    	position: currStat,
