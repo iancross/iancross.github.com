0135b5e6ffd4a33e6aa8280a4aae013fc8f97344
ian cross <ian.cross@tufts.edu>
2013-03-04 09:59:26 -0500

    HARD CODE ADDED

--- maps.js
+++ maps.js
@@ -78,7 +78,7 @@ function plotRed(){
 			        }
 			        parsed = JSON.parse(str);
 	        }*/
-			infowindow.setContent(this.title + '\n' + parsed)
+			infowindow.setContent("<p>" + this.title + "<br/>" + parsed "</p>")
 			infowindow.open(map,this)
 		})
 		i++;
