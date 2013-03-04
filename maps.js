4ddbe99185180643cf8bb778ce84d6040c28a6f6
ian cross <ian.cross@tufts.edu>
2013-03-04 13:47:09 -0500

    HARD CODE ADDED

--- maps.js
+++ maps.js
@@ -106,7 +106,9 @@ function parsing(){
 		str = request.responseText;
 		parsed = JSON.parse(str);
     }
+    RTInfo = ' ';
     for(i = 0; i<parsed.size; i++){
+    	console.log('for loop')
 	    if(parsed[i].PlatformKey==stationAbbrev.substring(0,stationAbbrev.length-1)){
 	    	console.log('fuck shit');
 		    RTInfo = parsed[i].PlatformKey[4] + ' ' + parsed[i].TimeRemaining + ' ';
