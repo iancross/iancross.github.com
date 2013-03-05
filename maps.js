7cf725056ac9e8767e91be6df4fa0c898f38c0d5
ian cross <ian.cross@tufts.edu>
2013-03-04 22:09:13 -0500

    working on Car and Wal

--- maps.js
+++ maps.js
@@ -91,7 +91,10 @@ function plotCar_Wal(){
 		console.log('yousuck')
 		return;
 	}*/
+	console.log('Carwal: ' Car_Wal)
+	console.log('Carmenfuck')
 	for (i=0;i<Car_Wal.length;i++) { 
+		console.log('fucktitties')
 		loc = Car_Wal[i].loc;
 		console.log(loc.latitude, loc.longitude);
 		currStat = new google.maps.LatLng(loc.latitude, loc.longitude);
